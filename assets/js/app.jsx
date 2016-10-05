1b887de46e1f8ca77d0314bb223b4084c0d12a95
Aleksey Lovchikov <svdmusic@gmail.com>
2016-10-05 11:04:55 +0300

    init commit

---
+++ assets/js/app.jsx
@@ -0,0 +1,149 @@
+var Note = React.createClass({
+	render() {
+		var style = {
+			backgroundColor: this.props.color
+		};
+		return (
+			<div className="note grid-item" style={style}>
+                <span className="delete" onClick={this.props.onDelete}>x</span>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+var NoteEditor = React.createClass({
+	getInitialState() {
+	    return {
+	        text: ''  
+	    };
+	},
+
+    handleChange() {
+        this.setState({
+            text: this.refs.noteText.value
+        });
+    },
+
+    handleNoteAdd() {
+        var newNote = {
+            text: this.state.text,
+            color: '#2c3e50',
+            id: Date.now()
+        };
+
+        if (this.state.text !== '') {
+            this.props.onNoteAdd(newNote);
+            this.setState({
+                text: ''
+            });
+        }
+    },
+
+	render() {
+		return (
+			<div className="note-editor">
+				<textarea
+					placeholder="Enter your note..."
+                    rows={5}
+                    ref="noteText"
+                    value={this.state.text}
+                    onChange={this.handleChange}
+                />
+				<button className="add-btn" onClick={this.handleNoteAdd}>ADD</button>
+			</div>
+		);
+	}
+});
+
+var NotesGrid = React.createClass({
+	componentDidMount() {
+		this.msnry = new Masonry(this.refs.notesGrid, {
+  			itemSelector: '.grid-item',
+			gutter: 10,
+			isFitWidth: true
+		});
+	},
+
+    componentDidUpdate(prevProps) {
+        if (this.props.notes.length !== prevProps.notes.length) {
+            this.msnry.reloadItems();
+            this.msnry.layout();
+        }
+    },
+
+	render() {
+        var onNoteDelete = this.props.onNoteDelete;
+		return (
+			<div className="notes-grid" ref="notesGrid">
+				{this.props.notes.map(function(note) {
+					return (
+                        <Note
+                            key={note.id}
+                            color={note.color}
+                            onDelete={onNoteDelete.bind(null, note)}
+                        >
+                            {note.text}
+                        </Note>
+                    );
+				})}
+			</div>
+		);
+	}
+});
+
+var App = React.createClass({
+	getInitialState() {
+	    return {
+	        notes: []
+	    };
+	},
+
+    componentDidMount() {
+        var localNotes = JSON.parse(localStorage.getItem('notes'));
+
+        if (localNotes) {
+            this.setState({
+                notes: localNotes
+            });
+        }
+    },
+
+    componentDidUpdate() {
+        this._updateLocalStorage();
+    },
+
+    handleDeleteNote(note) {
+        var noteId = note.id;
+        var newNotes = this.state.notes.filter((el) => {
+            return el.id !== noteId;
+        });
+        this.setState({
+            notes: newNotes
+        });
+    },
+
+    handleAddNote(note) {
+        var newNotes = this.state.notes.slice();
+        newNotes.unshift(note);
+        this.setState({
+            notes: newNotes
+        });
+    },
+
+	render() {
+		return (
+			<div className="notes-app">
+				<NoteEditor onNoteAdd={this.handleAddNote} />
+				<NotesGrid notes={this.state.notes} onNoteDelete={this.handleDeleteNote} />
+			</div>
+		);
+	},
+
+    _updateLocalStorage() {
+        var notes = JSON.stringify(this.state.notes);
+        localStorage.setItem('notes', notes);
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('app'));
