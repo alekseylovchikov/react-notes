74269eef06b3298ab0a4e1faea7e6acb0fc5e35d
Aleksey Lovchikov <svdmusic@gmail.com>
2016-10-05 11:34:12 +0300

    fix colors

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -26,7 +26,7 @@ var NoteEditor = React.createClass({
     },
 
     handleNoteAdd() {
-        let colors = ['#16a085', '#27ae60', '#2980b9', '#2c3e50', '#c0392b'];
+        let colors = ['#16a085', '#27ae60', '#2980b9', '#2c3e50', '#f39c12'];
         let randomColor = colors[Math.floor((Math.random() * colors.length - 1) + 1)];
         var newNote = {
             text: this.state.text,
